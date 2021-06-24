26f120b3527a2d36c081e13e6e2ccb4d51e74f8a
molML <francesca.grisoni@unimib.it>
2021-06-24 23:59:49 +0200

    mol desc

--- Lecture1_MolecularRepresentations/compute_count_descriptors.m
+++ Lecture1_MolecularRepresentations/compute_count_descriptors.m
@@ -2,13 +2,12 @@ function [descriptors, labels] = compute_count_descriptors(smiles)
 % insert description of the code function here
 % describe inputs and outputs
 
-nDB = []; % number of double bonds
 nO = []; % number of oxygen atoms
 nCl = [];  % number of chlorine atoms
 nC = []; % number of carbon atoms
 nN = []; % number of nitrogen atoms
 
 descriptors = [];
-labels = {'nDB' 'nO' 'nCl' 'nC' 'nN'};
+labels = {'nO' 'nCl' 'nC' 'nN'};
 
 end
--- Lecture1_MolecularRepresentations/compute_count_descriptors_solution.m
+++ Lecture1_MolecularRepresentations/compute_count_descriptors_solution.m
@@ -2,13 +2,12 @@ function [descriptors, labels] = compute_count_descriptors_solution(smiles)
 % insert description of the code function here
 % describe inputs and outputs
 
-nDB = numel(strfind(smiles,"=")); % number of double bonds
 nO = numel(strfind(smiles,"O")); % number of oxygen atoms
 nCl = numel(strfind(smiles,"Cl"));  % number of chlorine atoms
 nC = numel(strfind(smiles,"C"))+numel(strfind(smiles,"c"))-nCl; 
 nN = numel(strfind(smiles,"N"))+numel(strfind(smiles,"n")); 
 
-descriptors = [nDB nO nCl nC nN];
-labels = {'nDB' 'nO' 'nCl' 'nC' 'nN'};
+descriptors = [nO nCl nC nN];
+labels = {'nO' 'nCl' 'nC' 'nN'};
 
 end
