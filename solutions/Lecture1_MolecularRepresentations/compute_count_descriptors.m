058a8e2f9dc4b189da127743a996e41c4592eb2a
molML <francesca.grisoni@unimib.it>
2021-06-25 15:41:36 +0200

    folder with solutions

---
+++ solutions/Lecture1_MolecularRepresentations/compute_count_descriptors.m
@@ -0,0 +1,13 @@
+function [descriptors, labels] = compute_count_descriptors(smiles)
+% insert description of the code function here
+% describe inputs and outputs
+
+nO = []; % number of oxygen atoms
+nCl = [];  % number of chlorine atoms
+nC = []; % number of carbon atoms
+nN = []; % number of nitrogen atoms
+
+descriptors = [];
+labels = {'nO' 'nCl' 'nC' 'nN'};
+
+end
---
+++ solutions/Lecture1_MolecularRepresentations/compute_count_descriptors_solution.m
@@ -0,0 +1,13 @@
+function [descriptors, labels] = compute_count_descriptors_solution(smiles)
+% insert description of the code function here
+% describe inputs and outputs
+
+nO = numel(strfind(smiles,"O")); % number of oxygen atoms
+nCl = numel(strfind(smiles,"Cl"));  % number of chlorine atoms
+nC = numel(strfind(smiles,"C"))+numel(strfind(smiles,"c"))-nCl; 
+nN = numel(strfind(smiles,"N"))+numel(strfind(smiles,"n")); 
+
+descriptors = [nO nCl nC nN];
+labels = {'nO' 'nCl' 'nC' 'nN'};
+
+end
