7c02568375c54bb45cb3c7df96c970775dd2f79b
molML <francesca.grisoni@unimib.it>
2021-06-25 10:57:56 +0200

    distance exercise

--- Lecture1_MolecularRepresentations/compute_distance.m
+++ Lecture1_MolecularRepresentations/compute_distance.m
@@ -1,1 +1,19 @@
-function D = compute_distance(x1, x2)
+function D = compute_distance(x, y, dist_type)
+
+% describe your function here. It is good practice to explain the code
+% scopes, and the type/meaning of inputs and outputs
+
+switch dist_type
+   case 'euclidean'
+      % put your code here
+      
+   case 'manhattan'
+      % put your code here
+      
+   case 'lagrange'
+      % put your code here
+      
+   otherwise
+      disp 'Unknown type of distance selected'
+      D = [];
+end
